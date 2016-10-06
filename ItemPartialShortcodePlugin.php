29a60c6be50d0a85e5ba8a0297f7f7a7e9534c72
Steve Ranford <steve.ranford@warwick.ac.uk>
2016-10-06 10:55:50 +0100

    plugin written

---
+++ ItemPartialShortcodePlugin.php
@@ -0,0 +1,73 @@
+<?php     
+class FullItemShortcodePlugin extends Omeka_Plugin_AbstractPlugin {
+
+    protected $_hooks = array('initialize');
+
+      public function hookInitialize()
+    {
+        add_shortcode('fullitems', array($this, 'shortcodeFullItems'));
+    }
+
+    public function shortcodeFullItems($args, $view)
+    {
+        $params = array();
+
+        if (isset($args['is_featured'])) {
+            $params['featured'] = $args['is_featured'];
+        }
+
+        if (isset($args['has_image'])) {
+            $params['hasImage'] = $args['has_image'];
+        }
+
+        if (isset($args['collection'])) {
+            $params['collection'] = $args['collection'];
+        }
+
+        if (isset($args['item_type'])) {
+            $params['item_type'] = $args['item_type'];
+        }
+
+        if (isset($args['tags'])) {
+            $params['tags'] = $args['tags'];
+        }
+
+        if (isset($args['user'])) {
+            $params['user'] = $args['user'];
+        }
+
+        if (isset($args['ids'])) {
+            $params['range'] = $args['ids'];
+        }
+
+        if (isset($args['sort'])) {
+            $params['sort_field'] = $args['sort'];
+        }
+
+        if (isset($args['order'])) {
+            $params['sort_dir'] = $args['order'];
+        }
+
+        if (isset($args['template'])) {
+            $params['template'] = $args['template'];
+        } else {
+            $params['template'] = 'single';
+        }
+
+        if (isset($args['num'])) {
+            $limit = $args['num'];
+        } else {
+            $limit = 10; 
+        }
+
+        $items = get_records('Item', $params, $limit);
+
+        $content = '';
+        foreach ($items as $item) {
+           $content .= $view->partial('items/' . $params['template'] . '.php', array('item' => $item));
+        }
+
+        return $content;
+    }
+}
+?>
---
+++ README.md
@@ -0,0 +1,8 @@
+# omeka-plugin-ItemPartialShortcode
+
+This plugin extends the item shortcode to enable a new operator 'template' to specify a partial template other than 'single.php' in your theme to display the list of items.
+
+Instructions for use:
+- Copy single.php in your theme/items folder to custom-template-name.php
+- Edit custom-template-name.php to make any amendments to your theme's default 'single.php'
+- Use [itempartial template='custom-template-name'] along with other arguements in your shortcode use. Note, don't add .php to your template-shortcode. 
